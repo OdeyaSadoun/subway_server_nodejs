28de563c7ad423b1077cf3c9b3d384c6c3a04d9a
אודיה סעדון <osadoun@g.jct.ac.il>
2023-12-01 15:44:02 +0200

    add travel work

--- routes/travels.js
+++ routes/travels.js
@@ -1,8 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const { auth, authAdmin } = require("../middleware/auth");
+const { TravelModel } = require("../models/travelsModel");
+const { travelSchemaValidate } = require("../validations/travelsValidate");
 
-router.get("/", (req,res) => {
-    res.json({msg: "travels work project 3"});
+router.get("/", (req, res) => {
+  res.json({ msg: "travels work project 3" });
+});
+
+router.get("/travelsList", authAdmin, async (req, res) => {
+  try {
+    let data = await TravelModel.find({});
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "err", err });
+  }
+});
+
+router.post("/", auth,  async (req, res) => {
+  let validBody = travelSchemaValidate(req.body);
+  if (validBody.error) {
+    return res.status(400).json(validBody.error.details);
+  }
+  try {
+    let travel = new TravelModel(req.body);
+    travel.user_id = req.tokenData._id;
+    await travel.save();
+    res.status(201).json(travel);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "err", err });
+  }
 });
 
 module.exports = router;
--- validations/travelsValidate.js
+++ validations/travelsValidate.js
@@ -2,7 +2,6 @@ const Joi = require("joi");
 
 exports.travelSchemaValidate = (_reqBody) => {
   let travelValidate = Joi.object({
-    user_id: Joi.string().max(20).required(),
     train_name: Joi.string().max(3).required(),
     payment_type: Joi.string().max(50).required(),
   });
